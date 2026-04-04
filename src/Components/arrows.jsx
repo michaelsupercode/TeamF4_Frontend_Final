f2d98c1384ad217d5c3cc8aeaf0ec85b89e65e1c
michaelsupercode <michaelsupercode@gmail.com>
2026-04-04 15:56:01 +0200

    rep_set

--- src/Components/arrows.jsx
+++ src/Components/arrows.jsx
@@ -1,8 +1,11 @@
 import Arrows from '../Images/Arrows.png';
+import { Link } from 'react-router-dom';
 
 const ArrowsImg = () => {
   return (
-    <img className='arrows' src={Arrows} alt="arrows" />
+    <Link to="/cocktailList/filter.php%3Fi=Vodka" onClick={(event) => event.stopPropagation()}>
+      <img className='arrows' src={Arrows} alt="arrows" />
+    </Link>
   );
 }
 
